20c8fb444aca3a1432993161d67caf05d68e74f5
Paul Calande <paul.luap09@gmail.com>
2019-01-25 18:09:38 -0500

    git subrepo clone https://github.com/ByteNybbler/PAUtiLib ./Assets/Plugins/PAUtiLib
    subrepo:
      subdir:   "Assets/Plugins/PAUtiLib"
      merged:   "16c4335"
    upstream:
      origin:   "https://github.com/ByteNybbler/PAUtiLib"
      branch:   "master"
      commit:   "16c4335"
    git-subrepo:
      version:  "0.4.0"
      origin:   "???"
      commit:   "???"

---
+++ Assets/Plugins/PAUtiLib/Scripts/Mover/2D/RotateGraduallyToAngle2D.cs
@@ -0,0 +1,50 @@
+﻿// Author(s): Paul Calande
+// Rotates an object gradually to face a given angle.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGraduallyToAngle2D : MonoBehaviour
+{
+    [SerializeField]
+    TimeScale timeScale;
+    [SerializeField]
+    [Tooltip("The mover that will rotate the object.")]
+    Mover2D mover;
+    [SerializeField]
+    [Tooltip("The speed at which the object will rotate towards the velocity heading.")]
+    //float degreeChangePerSecond = 720.0f;
+    Angle angleChangePerSecond = Angle.FromDegrees(720.0f);
+    [SerializeField]
+    [Tooltip("The angle offset to use for rotating the sprite.")]
+    //float offsetDegrees;
+    Angle angleOffset;
+
+    // The degree measure of the target angle to approach.
+    //float targetDegrees = 0.0f;
+    Angle angleTarget = Angle.FromDegrees(0.0f);
+
+    // Sets the target angle to rotate to.
+    public void SetAngle(Angle angleTarget)
+        //float degrees)
+    {
+        //targetDegrees = degrees + offsetDegrees;
+        this.angleTarget = angleTarget + angleOffset;
+    }
+
+    private void UpdateCurrentDegreesHeading()
+    {
+        mover.TeleportRotation(mover.GetRotation().Approach(
+            angleTarget, angleChangePerSecond * timeScale.DeltaTime()));
+        /*
+            UtilApproach.AngleDegrees(mover.GetRotation(),
+            targetDegrees, degreeChangePerSecond * timeScale.DeltaTime()));
+            */
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateCurrentDegreesHeading();
+    }
+}
